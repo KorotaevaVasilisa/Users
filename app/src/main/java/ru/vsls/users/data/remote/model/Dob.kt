71e56115f0fa862dbc72ac0cd782b56c55e5196b
vsls <vasilisa_001@mail.ru>
2025-08-14 19:20:55 +0600

    Сформированы модели данных

---
+++ app/src/main/java/ru/vsls/users/data/remote/model/Dob.kt
@@ -0,0 +1,13 @@
+package ru.vsls.users.data.remote.model
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Dob(
+    @SerialName("age")
+    val age: Int,
+    @SerialName("date")
+    val date: String
+)
---
+++ app/src/main/java/ru/vsls/users/data/remote/model/Location.kt
@@ -0,0 +1,19 @@
+package ru.vsls.users.data.remote.model
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Location(
+    @SerialName("city")
+    val city: String,
+    @SerialName("country")
+    val country: String,
+    @SerialName("postcode")
+    val postcode: Int,
+    @SerialName("state")
+    val state: String,
+    @SerialName("street")
+    val street: Street
+)
