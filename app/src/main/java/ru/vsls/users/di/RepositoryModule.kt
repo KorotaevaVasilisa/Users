cd18fb9067cc686233d2f019f52cf93833445649
vsls <vasilisa_001@mail.ru>
2025-08-15 19:04:07 +0600

    Добавлен метод обновления данных во VM

--- app/src/main/java/ru/vsls/users/di/RepositoryModule.kt
+++ app/src/main/java/ru/vsls/users/di/RepositoryModule.kt
@@ -8,10 +8,13 @@ import ru.vsls.users.domain.repositories.DatabaseRepository
 import ru.vsls.users.domain.repositories.NetworkRepository
 import ru.vsls.users.domain.useCases.GetUsersUseCase
 import ru.vsls.users.domain.useCases.GetUsersUseCaseImpl
+import ru.vsls.users.domain.useCases.UpdateUsersUseCase
+import ru.vsls.users.domain.useCases.UpdateUsersUseCaseImpl
 
 val repositoryModule = module {
     //singleOf(::NetworkRepositoryImpl) bind NetworkRepository::class
     single<NetworkRepository> { NetworkRepositoryImpl(get()) }
     single<DatabaseRepository> { DatabaseRepositoryImpl(get()) }
     single<GetUsersUseCase> { GetUsersUseCaseImpl(get()) }
+    single<UpdateUsersUseCase> { UpdateUsersUseCaseImpl(get(), get()) }
 }
--- app/src/main/java/ru/vsls/users/presentation/screens/list/ListViewModel.kt
+++ app/src/main/java/ru/vsls/users/presentation/screens/list/ListViewModel.kt
@@ -6,25 +6,40 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import ru.vsls.users.domain.useCases.GetUsersUseCase
+import ru.vsls.users.domain.useCases.UpdateUsersUseCase
 
-class ListViewModel(private val getUsersUseCase: GetUsersUseCase): ViewModel() {
+class ListViewModel(
+    private val getUsersUseCase: GetUsersUseCase,
+    private val updateUsersUseCase: UpdateUsersUseCase,
+) : ViewModel() {
     private val _state = MutableStateFlow<ListUiState>(ListUiState())
     val state = _state.asStateFlow()
 
-    init{
-        loadRemoteUsers()
+    init {
+        loadLocalUsers()
     }
-    fun loadRemoteUsers() {
+
+    fun loadLocalUsers() {
         viewModelScope.launch {
             _state.value = _state.value.copy(isLoading = true, error = null)
-            try{
+            try {
                 val users = getUsersUseCase.invoke()
                 _state.value = ListUiState(users = users)
-            }
-            catch (e: Exception){
+            } catch (e: Exception) {
                 _state.value = _state.value.copy(isLoading = false, error = e.message)
             }
         }
+    }
 
+    fun updateUsers(){
+        viewModelScope.launch {
+            _state.value = _state.value.copy(isLoading = true, error = null)
+            try {
+                val users = updateUsersUseCase.invoke()
+                _state.value = ListUiState(users = users)
+            } catch (e: Exception) {
+                _state.value = _state.value.copy(isLoading = false, error = e.message)
+            }
+        }
     }
 }
