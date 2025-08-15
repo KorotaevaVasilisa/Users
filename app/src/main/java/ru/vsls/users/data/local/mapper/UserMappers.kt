d5734385a22bfdd2d52685c6e40332d7aae634fa
vsls <vasilisa_001@mail.ru>
2025-08-15 16:15:07 +0600

    добавлен маппер из entity в domain + удаление лишних моделей данных

--- app/src/main/java/ru/vsls/users/data/local/mapper/UserMappers.kt
+++ app/src/main/java/ru/vsls/users/data/local/mapper/UserMappers.kt
@@ -25,3 +25,26 @@ fun User.toEntity(): UserEntity {
         nat = nat,
     )
 }
+
+fun UserEntity.toDomain(): User{
+    return User(
+        id = id,
+        gender = gender,
+        title = title,
+        firstName = firstName,
+        lastName = lastName,
+        email = email,
+        phone = phone,
+        streetNumber = streetNumber,
+        streetName = streetName,
+        city = city,
+        state = state,
+        country = country,
+        pictureLarge = pictureLarge,
+        pictureMedium = pictureMedium,
+        pictureThumbnail = pictureThumbnail,
+        dobDateIso = dobDateIso,
+        dobAge = dobAge,
+        nat = nat,
+    )
+}
--- app/src/main/java/ru/vsls/users/domain/model/Location.kt
+++
@@ -1,8 +0,0 @@
-package ru.vsls.users.domain.model
-
-data class Location(
-    val city: String,
-    val country: String,
-    val state: String,
-    val street: Street,
-)
